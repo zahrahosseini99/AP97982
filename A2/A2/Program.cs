f8f90e362a462bda6e3460bb4e1ca025b966ef9f
zahra hosseini <hosseini99.zahra@gmail.com>
2019-03-16 12:08:58 +0330

    Implement A2

---
+++ A2/A2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+   public class Program
+    {
+        private static void Main(string[] args)
+        {
+           
+        }
+
+        public static void ArraySwap(ref int[] array1, ref int[] array2)
+        {
+            int[] tmp = array1;
+            array1 = array2;
+            array2 = tmp;
+            foreach(int c in array2)
+            Console.WriteLine(c);
+
+            
+        }
+
+        public static void ArraySwap(int[] array1, int [] array2)
+        {
+            int temp = 0;
+            for(int i = 0; i < array1.Length; i++)
+            {
+                
+                    temp = array1[i];
+                array1[i] = array2[i];
+                array2[i] = temp;
+               
+            }
+           
+        }
+
+        public static void AbsArray( int [] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = Math.Abs(array[i]);
+            }
+        }
+
+        public static void Append( ref int [] array , int f)
+        {
+            int[] arr = new int[array.Length + 1];
+           
+            int temp ;
+            for(int i = 0; i < array.Length; i++)
+            {
+                temp = array[i];
+                array[i] = arr[i];
+                arr[i] = temp;
+            }
+            arr[array.Length] = f;
+            array = arr;
+        }
+
+        public static void Sum(out int e,params int [] nums)
+        {
+             e=0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                e += nums[i];  
+            }
+        }
+
+        public static void Swap( ref int c, ref int d)
+        {
+            int temp = 0;
+            temp = c;
+            c = d;
+            d = temp;
+        }
+
+        public static void Square(ref int  b)
+        {
+             b= b * b;
+        }
+
+        public static void AssignPi(out double a)
+        {
+          a=Math.PI;
+        }
+    }
+}
