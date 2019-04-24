e8e2576ba0b0d35b70fb99d597b1638f9657da46
zahra hosseini <hosseini99.zahra@gmail.com>
2019-04-24 23:26:01 +0430

    review

--- A1S3/A1S3/Program .cs
+++ A1S3/A1S3/Program .cs
@@ -5,47 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApp22
+namespace A1S3
 {
     public class Program
     {
         static void Main(string[] args)
         {
 
-           
-           
+
+
             string pospath = @"C:\git\AP97982\A1S3\A1S3\TwitterData\Words\positive.txt";
             string[] posWords = Q1_GetWords(pospath);
 
             string negpath = @"C:\git\AP97982\A1S3\A1S3\TwitterData\Words\negative.txt";
             string[] negWords = Q1_GetWords(negpath);
 
-            string [] filePath=Directory.GetFiles(@"C:\git\AP97982\A1S3\A1S3\TwitterData\Tweets");
+            string[] filePath = Directory.GetFiles(@"C:\git\AP97982\A1S3\A1S3\TwitterData\Tweets");
             string[] data = new string[19];
             for (int i = 0; i < 19; i++)
             {
                 string tweetpath = filePath[i];
-                int index= tweetpath.LastIndexOf('\\');
-                string name1 = tweetpath.Substring(index+1);
+                int index = tweetpath.LastIndexOf('\\');
+                string name1 = tweetpath.Substring(index + 1);
                 int index1 = name1.LastIndexOf('.');
                 string name = name1.Substring(0, index1);
                 string[] tweets = File.ReadAllLines(tweetpath);
                 //string[] TweetAndMentions = alltweet.Split('\n');
                 //string tweet = Convert.ToString(tweets);
-                
-                   data[i]= name + ":" + Q5_GetAvgPopChargeOfTweets(tweets, negWords, posWords);
-               
-                
+
+                data[i] = name + ":" + Q5_GetAvgPopChargeOfTweets(tweets, negWords, posWords);
+
+
             }
             File.WriteAllLines(@"..\..\result.txt", data);
 
         }
         public static string[] Q1_GetWords(string path)
         {
-
-
             string content = File.ReadAllText(path);
             string[] words = content.Split('\n', '\r', ' ');
+
             return words;
         }
         public static bool Q2_IsInWords(string[] words, string word)
@@ -59,17 +58,10 @@ namespace ConsoleApp22
         }
         public static string[] Q3_GetWordsOfTweet(string tweet)
         {
-            List<string> res = new List<string>();
-            string[] tweetwords = tweet.Split('(', '.', '"', '!', '?', '#', '@', ',', ')', ':', ';', '*', '$', '%', '^', '&', '+', '-', '=', ' ', '،');
-            for (int i = 0; i < tweetwords.Length; i++)
-            {
-                if (tweetwords[i] != "")
-                {
-                    res.Add(tweetwords[i]);
-                }
-            }
 
-            return res.ToArray();
+            string[] str = tweet.Split(new char[] { ' ', '?', '@', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return str;
         }
         public static int Q4_GetPopChargeOfTweet(string tweet, string[] posWords, string[] negWords)
         {
@@ -100,10 +92,10 @@ namespace ConsoleApp22
             double countpos = 0;
             double countneg = 0;
             double sum = 0;
-            foreach(string tweetline in tweets)
+            foreach (string tweetline in tweets)
             {
                 var tweetWords = Q3_GetWordsOfTweet(tweetline);
-                foreach(string word in tweetWords)
+                foreach (string word in tweetWords)
                 {
                     if (negWords.Contains(word) && word != "")
                         countneg--;
