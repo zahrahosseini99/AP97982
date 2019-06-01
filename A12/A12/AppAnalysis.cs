738693a47caebded7007e6f3b37a6846155ca12a
zahra hosseini <hosseini99.zahra@gmail.com>
2019-06-01 17:32:43 +0430

    Implement HW12

---
+++ A12/A12/AppAnalysis.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.VisualBasic.FileIO;
+using System.Globalization;
+namespace A12
+{
+    public class AppAnalysis
+    {
+        public List<AppData> Apps;
+        public AppAnalysis(List<AppData> apps)
+        {
+            Apps = apps;
+        }
+
+        public static AppAnalysis AppAnalysisFactory(string appDataPath)
+        {
+            List<AppData> Apps = new List<AppData>();
+            var appAnalysis = new AppAnalysis(Apps);
+            using (TextFieldParser parser = new TextFieldParser(appDataPath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var fields = parser.ReadFields();
+                while (!parser.EndOfData)
+                {
+                    fields = parser.ReadFields();
+                    appAnalysis.AppendApp(fields);
+                }
+            }
+            return appAnalysis;
+        }
+
+        private void AppendApp(string[] fields)
+        {
+            AppData app = new AppData(fields);
+            Apps.Add(app);
+        }
+
+        public long AllAppsCount()
+        {
+            return Apps.Count();
+        }
+        public long AppsAboveXRatingCount(double x)
+        {
+            return Apps.Where(i => i.Rating >= x).Count();
+        }
+
+        public long RecentlyUpdatedCount(DateTime boundary)
+        {
+            return Apps.Where(i => i.LastUpdate >= boundary).Count();
+        }
+        public string RecentlyUpdatedFreqCat(DateTime boundary)
+        {
+            return Apps.Where(x => x.LastUpdate >= boundary).GroupBy(x => x.Categori)
+                    .OrderByDescending(grp => grp.Count())
+            .Select(grp => grp.Key).First();
+        }
+        public List<string> MostRatedCategories(double ratingBoundary, int n)
+        {
+            return Apps.Where(x => x.Rating > ratingBoundary).GroupBy(x => x.Categori)
+                           .OrderByDescending(g => g.Count()).Select(g => g.Key).Take(n).ToList();
+        }
+        public double TopQuarterBoundary()
+        {
+
+            int n = Apps.Where(i => i.Categori == "PHOTOGRAPHY").OrderBy(d => d.Rating).Count();
+            return Apps.Where(i => i.Categori == "PHOTOGRAPHY")
+                .OrderBy(d => d.Rating).Select(d => d.Rating).Take(3 * n / 4).Last();
+        }
+        public Tuple<string, string> ExtremeMeanUpdateElapse(DateTime today)
+        {
+            string first, last = null;
+            first = Apps.GroupBy(a => a.Categori).OrderBy(a => a.Average(i => (today - i.LastUpdate).TotalDays))
+                 .Select(i => i.Key).First();
+            last = Apps.GroupBy(a => a.Categori).OrderBy(a => a.Average(i => (today - i.LastUpdate).TotalDays))
+               .Select(i => i.Key).Last();
+            Tuple<string, string> res = new Tuple<string, string>(first, last);
+            return res;
+        }
+        public List<string> XMostProfitables(int x)
+        {
+            return Apps.OrderByDescending(i => i.Price * i.Installs).Take(x).Select(a => a.Name).ToList();
+        }
+        public List<string> XCoolestApps(int x, Func<AppData, double> criteria)
+        {
+            criteria = (app) => app.Rating * app.Installs / 1000;
+            return Apps.OrderBy(i => criteria(i)).Take(x).Select(i => i.Name).ToList();
+        }
+    }
+}
---
+++ A12/A12/AppData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace A12
+{
+    public class AppData
+    {
+        public string Name;
+        public string Categori;
+        public double Rating;
+        public long Reviews;
+        public double size;
+        public long Installs;
+        public string IsFree;
+        public double Price;
+        public string ContentRating;
+        public string Genres;
+        public DateTime LastUpdate;
+        public string CurrentVersion;
+        public string AndroidVersion;
+        public AppData(string[] fields)
+        {
+            Name = fields[0];
+            Categori = fields[1];
+            Rating = double.Parse(fields[2]);
+            Reviews = long.Parse(fields[3]);
+            if (fields[4] == "Varies with device")
+            {
+                fields[4] = "0";
+                size = double.Parse(fields[4].TrimEnd('M'));
+            }
+            else
+                size = double.Parse(fields[4].TrimEnd('M', 'k'));
+            Installs = int.Parse(fields[5].TrimStart('"').TrimEnd('"').TrimEnd('+'), NumberStyles.AllowThousands);
+            IsFree = fields[6];
+            Price = double.Parse(fields[7].TrimStart('$'));
+            ContentRating = fields[8];
+            Genres = fields[9];
+            LastUpdate = DateTime.Parse(fields[10]);
+            if (fields[11] == "Varies with device")
+            {
+                fields[11] = "0";
+                CurrentVersion = fields[11];
+            }
+            else
+                CurrentVersion = fields[11];
+            if (fields[12] == "Varies with device")
+            {
+                fields[12] = "0";
+                AndroidVersion = fields[12];
+            }
+            else
+                AndroidVersion = fields[12];
+
+        }
+    }
+}
